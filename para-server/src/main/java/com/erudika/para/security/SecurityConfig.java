40db9b85d36514f9831636019a7eaf87888e8449
周志臻 <1259203572@qq.com>
2018-12-14 14:02:09 +0800

    添加微信绑定servlet

--- para-server/src/main/java/com/erudika/para/security/SecurityConfig.java
+++ para-server/src/main/java/com/erudika/para/security/SecurityConfig.java
@@ -132,6 +132,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		web.ignoring().requestMatchers(IgnoredRequestMatcher.INSTANCE, new AntPathRequestMatcher("/active/**"));
 		web.ignoring().requestMatchers(IgnoredRequestMatcher.INSTANCE, new AntPathRequestMatcher("/trialRegister/**"));
 		web.ignoring().requestMatchers(IgnoredRequestMatcher.INSTANCE, new AntPathRequestMatcher("/insiderLogin/**"));
+		web.ignoring().requestMatchers(IgnoredRequestMatcher.INSTANCE, new AntPathRequestMatcher("/wechat/**"));
 		DefaultHttpFirewall firewall = new DefaultHttpFirewall();
 		firewall.setAllowUrlEncodedSlash(true);
 		web.httpFirewall(firewall);
